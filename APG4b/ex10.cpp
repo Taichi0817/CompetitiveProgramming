3791fcf8c2b687e390fe9551aef676324c1a537e
Agenasu0817 <sfjt9343ddq22022@gmail.com>
2023-05-12 17:09:15 +0900

    firstcommit

---
+++ APG4b/ex10.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+  cout << "A:";
+  while(A) {
+    cout << "]";
+    A--;
+  }
+  cout << endl;
+  cout << "B:";
+  while(B) {
+    cout << "]";
+    B--;
+  }
+  cout << endl;
+}
