fc589525ef06c149743aa53421f3d9cd11ff6822
Taichi0817 <sfjt9343ddq22022@gmail.com>
2023-05-27 18:51:56 +0900

    learned hani for

---
+++ abc_lesson/looptest.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int N = 5;
+    vector <int> s(N);
+    for (auto &a : s) {
+        cin >> a;
+    }
+    for (auto a : s) {
+        cout << a << endl;
+    }
+}
