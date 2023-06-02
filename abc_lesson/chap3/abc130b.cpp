941d2dd11d74046dd67b482ee49a45bf311dca01
Taichi0817 <sfjt9343ddq22022@gmail.com>
2023-06-02 23:34:25 +0900

    learned recurrence formula

--- abc_lesson/chap3/abc130b.cpp
+++ abc_lesson/chap3/abc130b.cpp
@@ -1,21 +1,20 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define rep(i, a, b) for(int i=a;i<b;i++)
-#define rrep(i, a, b) for(int i=a;i>=b;i--)
 
-
-int main (void) {
+int main(void) {
+    
     int N;
     int X;
-    int L[100];
+    int L[102];
 
     cin >> N >> X;
-    rep(i, 0, N) cin >> L[i];
-    
-    int d = 0; int ans = 1;
-    rep(i, 0, N) {
-        d += L[i];
-        if (X < d) break;
+    rep(n, 0, N) cin >> L[n];
+
+    int D = 0;  int ans = 1;
+    rep(n, 0, N) {
+        D += L[n];
+        if(X < D) break;
         ans++;
     }
     cout << ans << endl;
