e42e4cf4d5ffd3816f60231b4142688e5aeec4d4
Taichi0817 <sfjt9343ddq22022@gmail.com>
2023-06-13 22:57:03 +0900

    "tired"

---
+++ itp1/1_3_b.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int i=1;
+    while(1) {
+        int n;
+        cin >> n;
+        if(!n) break;
+        cout << "Case " << i << ": " << n << endl;
+        i++;
+    }
+}
