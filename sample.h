93781be3dd8ad6f1cc503556821f1891a79522ee
Agenasu0817 <sfjt9343ddq22022@gmail.com>
2023-06-17 14:18:54 +0900

    tired

--- sample.h
+++ sample.h
@@ -9,3 +9,4 @@ void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
 typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
 template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
